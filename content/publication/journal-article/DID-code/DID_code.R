4d663db2f4681365571347ad26d1b6f5582fb00e
Alven8816 <yuwenhuajiayou@126.com>
2020-07-22 12:46:16 +1000

    condition for using code
    20200722

--- content/publication/journal-article/DID-code/DID_code.R
+++ content/publication/journal-article/DID-code/DID_code.R
@@ -1,10 +1,22 @@
+####========
+# Conditions for using the code
+#
+# If you publish any work using or referring the code,
+# please cite the publication (Yu et al., 2020) in the following recommended format:
+#
+# Yu, Wenhua, Yuming Guo, Liuhua Shi, and Shanshan Li. "The association
+# between long-term exposure to low-level PM2. 5 and mortality in the
+# state of Queensland, Australia: A modelling study with the difference-in-differences
+# approach." PLoS medicine 17, no. 6 (2020): e1003141.
+#========###
+
 library(dplyr)
 library(splines)
 library(mgcv)
 library(gnm)
-
+#load the example data
 mydata <- read.csv("example.csv")
-
+# model establishment
 for (i in c(8,27,36,53)){
         model <- gnm(mydata[, i] ~ bs(pm2_5) + year + bs(win_temp_mean,4) + bs(sum_temp_mean,4) +win_temp_sd + sum_temp_sd + SEIFA,
                      data=mydata, family=poisson, offset = log(population),
